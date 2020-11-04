48abe0b9102b59cd0a08a2d0f7f474b6c10d86a2
Clement <clemwek@gmail.com>
2020-11-04 10:46:32 +0300

    Initial Commit

---
+++ appStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appStudy
+//
+//  Created by Clement  Wekesa on 04/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
