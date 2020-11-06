31c0465edae3078300e5eb4c73b5a4debc91b672
Clement <clemwek@gmail.com>
2020-11-06 09:23:55 +0300

    Add a web view

---
+++ appStudy/Help/HelpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HelpViewController.swift
+//  appStudy
+//
+//  Created by Clement  Wekesa on 06/11/2020.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        webView.loadRequest(URLRequest(url: URL(string: "https://www.apple.com/")!))
+    }
+
+}
